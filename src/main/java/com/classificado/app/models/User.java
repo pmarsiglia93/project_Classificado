f398271da4eee9b4425b4a445f0baba64d6cb4ba
Paulo <pmarsiglia93@gmail.com>
2022-01-21 14:52:07 -0300

    @pmarsiglia93 [Task: DOCUMENTATION User Model]

--- src/main/java/com/classificado/app/models/User.java
+++ src/main/java/com/classificado/app/models/User.java
@@ -8,18 +8,41 @@ import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+/**
+ * Java - Doc
+ * Classe Modelo para persistência no Banco de Dados.
+ * 
+ * <p>
+ * - idUser: Chave primaria da tabela. Tipo Long
+ * </p>
+ * <p>
+ * - name: Coluna nome da tabela tb_user. Tipo String
+ * </p>
+ * <p>
+ * - name: Coluna nome da tabela tb_user. Tipo String
+ * </p>
+ * @author Gustavo Boaz
+ * @author Paulo Francisco
+ * @since 21/01/2022
+ * @see UserRepository
+ * @see UserController
+ */
+
 @Entity
 @Table(name = "tb_users")
 public class User {
 	
 	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUser;
 	private String name;
-	private @NotBlank @Email String email;
+	private @NotBlank @Email String email; //Aqui está um comentário
 	private String password;
+	private Long idade;
+	
 	
 	public Long getIdUser() {
 		return idUser;
 	}
+	
 	public void setIdUser(Long idUser) {
 		this.idUser = idUser;
 	}
